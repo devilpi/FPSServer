57898e99b22f131753477f3a85b828be3d9145ad
devilpi <517348646@qq.com>
2018-06-18 18:19:13 +0800

    update dead1

--- app.js
+++ app.js
@@ -392,7 +392,7 @@ var rayCaster = new THREE.Raycaster();
 module.exports = app;
 
 // player status
-var DEADTIME = 2900; // ms
+var DEADTIME = 1000; // ms
 var STRONGTIME = 1000; // ms
 var OFFSET = 10.25;
 var INFINITY = 1000;
